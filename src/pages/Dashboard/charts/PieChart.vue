45c1c201149d8444d1e90a606e82b9654047ed4e
baiyichen <chenbaiyi.work@outlook.com>
2024-01-27 22:22:04 +0800

    add: 1.首页redis监控图标
    Signed-off-by: baiyichen <chenbaiyi.work@outlook.com>

--- src/pages/Dashboard/charts/PieChart.vue
+++ src/pages/Dashboard/charts/PieChart.vue
@@ -10,7 +10,11 @@ import {markRaw, onMounted, ref, watch} from 'vue';
 import useTheme from 'src/composables/useTheme'
 // const echarts = require('echarts')
 import * as echarts from "echarts";
+import {getAction} from "src/api/manage"
 
+const url = {
+  command: 'public/command',
+}
 const {darkThemeChart} = useTheme()
 const chart = ref(null)
 const piechart = ref(null)
@@ -21,53 +25,143 @@ onMounted(() => {
 watch(() => darkThemeChart.value, () => {
   init()
 })
-const init = () => {
+const init = async () => {
   let ct = piechart.value;
   echarts.dispose(ct);
   chart.value = markRaw(echarts.init(ct, darkThemeChart.value));
-  chart.value.setOption(options.value);
+  const res = await getAction(url.command)
+  let options = {}
+  if (res.code === 1) {
+    let dataSet = [
+      {
+        value: res.data.info.connected_clients,
+        name: '客户端连接数量',
+      },
+      {
+        value: res.data.info.used_memory_peak_perc.replace('%', ''),
+        name: '内存使用峰值',
+      },
+      {
+        value: res.data.info.mem_fragmentation_ratio,
+        name: '内存碎片化比例',
+      }
+    ]
+    options = getOption(dataSet)
+  } else {
+    options = getOption(dataSet.value)
+  }
+
+
+  chart.value.setOption(options);
 }
 const onResize = () => {
   chart.value?.resize()
 }
 
-const options = ref({
-  tooltip: {
-    trigger: 'item'
-  },
-  legend: {
-    top: '5%',
-    left: 'center',
-  },
-  series: [
-    {
-      name: 'Access From',
-      type: 'pie',
-      radius: ['40%', '70%'],
-      avoidLabelOverlap: false,
-      label: {
-        show: false,
-        position: 'center'
-      },
-      emphasis: {
-        label: {
-          show: true,
-          fontSize: '40',
-          fontWeight: 'bold'
-        }
-      },
-      labelLine: {
-        show: false
-      },
-      data: [
-        {value: 1048, name: 'Search Engine'},
-        {value: 735, name: 'Direct'},
-        {value: 580, name: 'Email'},
-        {value: 484, name: 'Union Ads'},
-        {value: 300, name: 'Video Ads'}
-      ]
-    }
-  ]
-})
 
+const dataSet = ref([
+  {
+    value: 20,
+    name: 'Perfect',
+  },
+  {
+    value: 40,
+    name: 'Good',
+  },
+  {
+    value: 60,
+    name: 'Commonly',
+  }
+])
+const getOption = (data) => {
+  return {
+    series: [
+      {
+        type: 'gauge',
+        startAngle: 90,
+        endAngle: -270,
+        pointer: {
+          show: false
+        },
+        progress: {
+          show: true,
+          overlap: false,
+          roundCap: true,
+          clip: false,
+          itemStyle: {
+            borderWidth: 1,
+            borderColor: '#464646'
+          }
+        },
+        axisLine: {
+          lineStyle: {
+            width: 40
+          }
+        },
+        splitLine: {
+          show: false,
+          distance: 0,
+          length: 10
+        },
+        axisTick: {
+          show: false
+        },
+        axisLabel: {
+          show: false,
+          distance: 50
+        },
+        data: [
+          {
+            value: data[0].value,
+            name: data[0].name,
+            title: {
+              offsetCenter: ['0%', '-30%']
+            },
+            detail: {
+              valueAnimation: true,
+              offsetCenter: ['0%', '-15%'],
+              formatter: '{value}台'
+            }
+          },
+          {
+            value: data[1].value,
+            name: data[1].name,
+            title: {
+              offsetCenter: ['0%', '0%']
+            },
+            detail: {
+              valueAnimation: true,
+              offsetCenter: ['0%', '15%'],
+              formatter: '{value}%'
+            }
+          },
+          {
+            value: data[2].value,
+            name: data[2].name,
+            title: {
+              offsetCenter: ['0%', '30%']
+            },
+            detail: {
+              valueAnimation: true,
+              offsetCenter: ['0%', '45%'],
+              formatter: '{value}%'
+            }
+          }
+        ],
+        title: {
+          fontSize: 18
+        },
+        detail: {
+          width: 50,
+          height: 10,
+          fontSize: 14,
+          color: 'inherit',
+          borderColor: 'inherit',
+          borderRadius: 20,
+          borderWidth: 1,
+        }
+      }
+    ]
+  }
+}
 </script>
--- src/pages/Dashboard/charts/PieChart2.vue
+++ src/pages/Dashboard/charts/PieChart2.vue
@@ -6,64 +6,74 @@
 </template>
 
 <script setup>
+import {getAction} from "src/api/manage"
 import {markRaw, onMounted, ref, watch} from 'vue';
 import useTheme from 'src/composables/useTheme'
 // const echarts = require('echarts')
 import * as echarts from "echarts";
 
+const url = {
+  command: 'public/command',
+}
 const {darkThemeChart} = useTheme()
 const chart = ref(null)
 const piechart = ref(null)
-
 onMounted(() => {
   init()
 })
 watch(() => darkThemeChart.value, () => {
   init()
 })
-const init = () => {
+const dataSet = ref([
+  {value: 1048, name: 'Search Engine'},
+  {value: 735, name: 'Direct'},
+  {value: 580, name: 'Email'},
+  {value: 484, name: 'Union Ads'},
+  {value: 300, name: 'Video Ads'}
+])
+const getOption = (data) => {
+  return {
+    toolbox: {
+      show: true,
+      feature: {
+        mark: {show: true},
+        dataView: {show: true, readOnly: false},
+        restore: {show: true},
+        saveAsImage: {show: true}
+      }
+    },
+    series: [
+      {
+        name: 'Nightingale Chart',
+        type: 'pie',
+        radius: [50, 150],
+        center: ['50%', '50%'],
+        roseType: 'area',
+        itemStyle: {
+          borderRadius: 8
+        },
+        data: data
+      }
+    ]
+  }
+}
+const init = async () => {
   let ct = piechart.value;
   echarts.dispose(ct);
   chart.value = markRaw(echarts.init(ct, darkThemeChart.value));
-  chart.value.setOption(options.value);
+  const res = await getAction(url.command)
+  let options = {}
+  if (res.code === 1) {
+    options = getOption(res.data.commandStats)
+  } else {
+    options = getOption(dataSet.value)
+  }
+
+  chart.value.setOption(options);
 }
 const onResize = () => {
   chart.value?.resize()
 }
 
-const options = ref({
-  legend: {
-    top: 'bottom'
-  },
-  toolbox: {
-    show: true,
-    feature: {
-      mark: {show: true},
-      dataView: {show: true, readOnly: false},
-      restore: {show: true},
-      saveAsImage: {show: true}
-    }
-  },
-  series: [
-    {
-      name: 'Nightingale Chart',
-      type: 'pie',
-      radius: [50, 150],
-      center: ['50%', '50%'],
-      roseType: 'area',
-      itemStyle: {
-        borderRadius: 8
-      },
-      data: [
-        {value: 40, name: 'rose 1'},
-        {value: 38, name: 'rose 2'},
-        {value: 32, name: 'rose 3'},
-        {value: 30, name: 'rose 4'},
-        {value: 28, name: 'rose 5'},
-        {value: 26, name: 'rose 6'},
-      ]
-    }
-  ]
-})
 
 </script>
