9faf724a5483ee7954178085e6e7ce3b718bb647
baiyichen <chenbaiyi.work@outlook.com>
2024-03-28 02:34:18 +0800

    feat(课程页面): 课程页面数据填充
    Signed-off-by: baiyichen <chenbaiyi.work@outlook.com>

--- src/main/java/site/cilicili/frontend/course/domain/dto/QueryCourseInfoRequest.java
+++ src/main/java/site/cilicili/frontend/course/domain/dto/QueryCourseInfoRequest.java
@@ -4,6 +4,10 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
 /**
  * PROJECT:cilicili
  * PACkAGE:site.cilicili.frontend.course.domain.dto
@@ -20,5 +24,12 @@ public class QueryCourseInfoRequest {
     private @NotNull Integer pageSize;
     private String sortBy;
     private String status;
+    private String status1;
     private String kind;
+    private String teacher;
+    private List<String> s;
+
+    public List<String> getS() {
+        return Optional.ofNullable(status1).map(status -> "yes".equals(status.split("_")[1]) ? List.of("onOffPass_on") : Arrays.asList("onOffPass_off", "onOffPass_pass")).orElse(List.of(""));
+    }
 }
